55e5710bf6db0941d8833e7f120b347a0e76b674
parth thakor <parththakor2610@gmail.com>
2023-08-11 14:59:52 +0530

    bodyparser

--- second/practiseeset02.js
+++ second/practiseeset02.js
@@ -1,15 +1,36 @@
-const express = require('express');
+const express= require('express');
+const bodyParser = require('body-parser');
+const app  =  express();
+//this is the simple type
+// app.use((req,res,next)=>{
+//   console.log("this is the first marvell")
+//   next();
+// })
+// app.use((req,res,next)=>{
+//   console.log("this is second marvell")
+//   res.send('<h1>hellow from Express</h1>')
+// })
 
-const app = express();
+ //parse the data 
+ app.use(bodyParser.urlencoded({extended : false}))
+//use middleware with routes 
+app.use('/' , (req,res,next)=>{
+  // console.log("run")
+  next();
+})
+app.use('/add-product' , (req,res,next)=>{
+  // console.log('product details middleware');
+  res.send('<form action="/product" method="POST"><input type="text" name="product"><input type="number" name="size"><button>Add Product</button></form>')
+})
+app.use('/product',(req,res)=>{
+      console.log(req.body)
+      res.redirect('/')
 
-app.use((req, res, next) => {
-  console.log('In the middleware!');
-  next(); // Allows the request to continue to the next middleware in line
-});
+})
+app.use('/' ,(req,res,next)=>{
+  // console.log("this is second marvell")
+  res.send('<h1>hellow from Express</h1>')
+})
 
-app.use((req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Hello from Express!</h1>');
-});
 
-app.listen(3000);
+app.listen(3000)
